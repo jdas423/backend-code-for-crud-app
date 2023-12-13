217612cee60c1ead7745731709dd26fae138d0e6
jagdish das <jagdishdas.aps@gmail.com>
2023-12-13 09:40:51 +0530

    Update users.js

--- users.js
+++ users.js
@@ -2,10 +2,19 @@ const express=require('express');
 const { wrapAsync, appError } = require('./errorhandling');
 const router=express.Router();
 const {User,validateUser,validateUpdation}=require('./usersmodel');
+let lastIndex;
 
+User.find({}).then(users => {
+    lastIndex= users.length;
+    console.log(lastIndex)
+})
+.catch(err => {
+    console.error('Could not get users...',err)
+})
 
 const passer=(validateUser)=>{
     return (req,res,next)=>{
+
         req.body.id=lastIndex+1;
         try{
           validateUser(req,res);
@@ -33,11 +42,11 @@ const updater=(validateUpdation)=>{
 router.get("/",wrapAsync(async(req,res,next)=>{
     if(!Object.keys(req.query).length){
         const sInd= 1;
-        const eInd= 20;   
+        const eInd= 10;   
         const users= await User.find({$and : [{id: {$gte: sInd}},{id: {$lte: eInd}}]});
         return res.status(200).json(users);
     }
-    if(!req.query.hasOwnProperty("pages")) throw new appError(500,"Invalid query");
+    if(!req.query.hasOwnProperty("pages")) throw new appError(400,"Invalid query");
     next();  
 }))
 
@@ -49,12 +58,12 @@ router.get("/",wrapAsync(async(req,res,next)=>{
         res.redirect("/api/users?pages=1");
     }
     if(pages<1){
-        throw new appError(500,"Invalid Page Number");
+        throw new appError(400,"Invalid Page Number");
     }
     const sInd= (pages-1)*10+1;
     const eInd= pages*10;   
     const users= await User.find({$and : [{id: {$gte: sInd}},{id: {$lte: eInd}}]});
-    if(!users.length) throw new appError(400,"page Not Found");
+    if(!users.length) throw new appError(404,"page Not Found");
     res.status(200).json(users);
 }))
 
